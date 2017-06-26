523abddabe2ac8d14c04d60bbc27502e78a20c7c
Radoslaw Marek <Radoslaw.Marek@luxmed.pl>
2017-06-26 11:34:10 +0200

    Dodanie konfiguracji UglifyJS do webpacka

--- WebPack/webpack.config.js
+++ WebPack/webpack.config.js
@@ -1,4 +1,5 @@
 var path = require('path');
+var webpack = require('webpack');
 
 module.exports = {
     entry: './app/app.js',
@@ -17,5 +18,10 @@ module.exports = {
                 ]
             }
         ]
-    }
+    },
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin({
+
+        })
+    ]
 };
